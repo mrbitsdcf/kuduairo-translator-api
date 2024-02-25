379dd07dcacf4365667aea292165b1f0c4bc9580
Diogo Fernandes <diogo@nubeliu.com>
2024-02-25 09:07:50 -0300

    Fixes via pre-commit

--- server/api.py
+++ server/api.py
@@ -1,26 +1,27 @@
+from __future__ import annotations
+
 from fastapi import FastAPI
-from .routes import router as TranslateRouter
 from fastapi.middleware.cors import CORSMiddleware
+
+from .routes import router as TranslateRouter
 
 app = FastAPI()
 
 origins = [
-    "https://kuduairo.netlify.app",
-    "http://192.168.1.70:3000",
-    "http://localhost:3000",
+    'http://localhost:3000',
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 
-@app.get("/", tags=["Root Path"])
+@app.get('/', tags=['Root Path'])
 async def index():
     return {'status': 'OK'}
 
-app.include_router(TranslateRouter, prefix="/translate")
+app.include_router(TranslateRouter, prefix='/translate')
